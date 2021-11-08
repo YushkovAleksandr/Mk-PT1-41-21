6ed1e9808fdfadd0cff5659d87da34bdbf29da0d
Nick <77110489+nickkuchko@users.noreply.github.com>
2021-11-08 10:17:12 +0300

    HW 5
    Create HW5

---
+++ Tasks/Kuchko/Test_5.3.py
@@ -0,0 +1,23 @@
+# 3. Написать функцию для вычисления n первых чисел Фибоначчи.
+# Примеры вызова: 
+# fib(5) -> 0,1,1,2,3
+# fib(10) -> 0,1,1,2,3,5,8,13,21,34 
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+
+if __name__ ==  "__main__":
+    
+    a = int(input("Введите количество первых чисел Фибоначчи: "))
+
+    result = []
+    start = 0
+    for i in range(start, a):
+        result.append(fibonacci(i))
+    print(', '.join(str(x) for x in result))
